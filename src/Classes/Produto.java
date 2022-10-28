2ad8d53f27d314cec905a139e50187499e52bc97
Gr-monaco <gr-monaco@hotmail.com>
2022-10-27 21:05:43 -0300

    Adiciona produto

---
+++ src/Classes/Produto.java
@@ -0,0 +1,31 @@
+package Classes;
+
+public class Produto {
+
+    private String nome;
+    private Float preco;
+
+    public Produto() {
+    }
+
+    public Produto(String nome, Float preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+}
