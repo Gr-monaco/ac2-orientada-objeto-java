393753f0e54814f9e4d6b810a81afa9012a97f78
Gr-monaco <gr-monaco@hotmail.com>
2022-11-07 21:40:06 -0300

    Adicionar arrayList para simular persistencia

--- src/Service/FornecedorService.java
+++ src/Service/FornecedorService.java
@@ -6,16 +6,11 @@ import java.util.Scanner;
 
 public class FornecedorService {
 
-    public Fornecedor criaFornecedor() {
-
-        Scanner scanner = new Scanner(System.in);
-
+    public Fornecedor criaFornecedor(Scanner scanner) {        
         System.out.println("Insira um nome:");
         String nome = scanner.nextLine();
         System.out.println("Insira  CPNJ:");
         String CNPJ = scanner.nextLine();
-
-        scanner.close();
 
         return new Fornecedor(nome, CNPJ);
     }
