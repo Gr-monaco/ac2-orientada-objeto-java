ed08f4af11c1cc41ad5d1deebed41445d312bac5
Gr-monaco <gr-monaco@hotmail.com>
2022-11-15 14:55:44 -0300

    Cadastrar pessoa juridica

--- src/ClienteService.java
+++ src/ClienteService.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 import Classes.PessoaFisica;
+import Classes.PessoaJuridica;
 
 public class ClienteService {
   public PessoaFisica criaClientePessoaFisica(Scanner scanner){
@@ -13,5 +14,16 @@ public class ClienteService {
 
     return new PessoaFisica(nome, email, cpf);
 
+  }
+
+  public PessoaJuridica criaClientePessoaJuridica(Scanner scanner){
+    System.out.println("Insira um nome:");
+    String nome = scanner.nextLine();
+    System.out.println("Insira um email:");
+    String email = scanner.nextLine(); 
+    System.out.println("Insira um CNPJ:");
+    String cnpj = scanner.nextLine();
+
+    return new PessoaJuridica(nome, email, cnpj);
   }
 }
