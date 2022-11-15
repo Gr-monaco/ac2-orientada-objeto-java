5adc0d8a1a62fc039bff95a4868517074237d800
Gr-monaco <gr-monaco@hotmail.com>
2022-11-15 18:46:48 -0300

    Adicionar cadastro de produto

---
+++ src/Service/ProdutoService.java
@@ -0,0 +1,34 @@
+package Service;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Classes.Fornecedor;
+import Classes.Produto;
+
+public class ProdutoService {
+  public Produto cadastrarProduto(Scanner scanner, ArrayList<Fornecedor> listaDeFornecedores){
+    System.out.println("Escreva um nome:");
+    String nome = scanner.nextLine();
+    System.out.println("Descrição:");
+    String descricao = scanner.nextLine();
+    System.out.println("Valor unitario:");
+    //TODO: try catch
+    Float valorUnitario = Float.parseFloat(scanner.nextLine()); // vai dar expection se não for float
+    
+    System.out.println("Escolha um fornecedor:");
+
+    for(int i = 0; i < listaDeFornecedores.size(); i++){
+      System.out.println("["+(i+1)+"] - " + listaDeFornecedores.get(i).toString());
+    }
+
+    System.out.println("Escolha um numero: ");
+
+    //TODO: try catch
+    int escolha = Integer.parseInt(scanner.nextLine());
+
+    Fornecedor fornecedorEscolhido = listaDeFornecedores.get(escolha);
+
+    return new Produto(nome, valorUnitario, descricao, fornecedorEscolhido);
+  }
+}
