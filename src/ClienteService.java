5c1e8f33149455f637c554494fb76f7b2896c38d
Gr-monaco <gr-monaco@hotmail.com>
2022-11-15 15:06:01 -0300

    Escolher tipo de cliente

--- src/ClienteService.java
+++ src/ClienteService.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+import Classes.Cliente;
 import Classes.PessoaFisica;
 import Classes.PessoaJuridica;
 
@@ -25,5 +26,21 @@ public class ClienteService {
     String cnpj = scanner.nextLine();
 
     return new PessoaJuridica(nome, email, cnpj);
+  }
+
+  public Cliente escolherTipoDeCliente(Scanner scanner){
+    System.out.println("""
+        [1] - Pessoa Física
+        [2]  - Juridica
+        """);
+    String escolha = scanner.nextLine().replaceAll("\\[", "").replaceAll("]", "");  
+    if(escolha=="1"){
+      return this.criaClientePessoaFisica(scanner);
+    } else if (escolha=="2"){
+      return this.criaClientePessoaJuridica(scanner);
+    }else{ //Isso vai fazer o menu aparecer de novo caso não escolha uma
+           //opção valida, não é mto elegante mas deve funcionar
+      return this.escolherTipoDeCliente(scanner);
+    }
   }
 }
